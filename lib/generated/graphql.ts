45ed630a36c9cfbd0036c06b5a2c17a6f67510a7
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2021-09-18 11:44:01 +0900

    create form

--- lib/generated/graphql.ts
+++ lib/generated/graphql.ts
@@ -72,6 +72,17 @@ export type SlugsQueryVariables = Exact<{ [key: string]: never; }>;
 
 export type SlugsQuery = { __typename?: 'Query', posts: Array<{ __typename?: 'Post', slug: string }> };
 
+export type CreatePostMutationVariables = Exact<{
+  slug: Scalars['String'];
+  title: Scalars['String'];
+  coverImage: Scalars['String'];
+  content: Scalars['String'];
+  description: Scalars['String'];
+}>;
+
+
+export type CreatePostMutation = { __typename?: 'Mutation', createPost: { __typename?: 'Post', id: string } };
+
 
 export const PostDocument = gql`
     query post($slug: String!) {
@@ -149,3 +160,42 @@ export function useSlugsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<Slug
 export type SlugsQueryHookResult = ReturnType<typeof useSlugsQuery>;
 export type SlugsLazyQueryHookResult = ReturnType<typeof useSlugsLazyQuery>;
 export type SlugsQueryResult = Apollo.QueryResult<SlugsQuery, SlugsQueryVariables>;
+export const CreatePostDocument = gql`
+    mutation createPost($slug: String!, $title: String!, $coverImage: String!, $content: String!, $description: String!) {
+  createPost(
+    input: {slug: $slug, title: $title, coverImage: $coverImage, content: $content, description: $description}
+  ) {
+    id
+  }
+}
+    `;
+export type CreatePostMutationFn = Apollo.MutationFunction<CreatePostMutation, CreatePostMutationVariables>;
+
+/**
+ * __useCreatePostMutation__
+ *
+ * To run a mutation, you first call `useCreatePostMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreatePostMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createPostMutation, { data, loading, error }] = useCreatePostMutation({
+ *   variables: {
+ *      slug: // value for 'slug'
+ *      title: // value for 'title'
+ *      coverImage: // value for 'coverImage'
+ *      content: // value for 'content'
+ *      description: // value for 'description'
+ *   },
+ * });
+ */
+export function useCreatePostMutation(baseOptions?: Apollo.MutationHookOptions<CreatePostMutation, CreatePostMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreatePostMutation, CreatePostMutationVariables>(CreatePostDocument, options);
+      }
+export type CreatePostMutationHookResult = ReturnType<typeof useCreatePostMutation>;
+export type CreatePostMutationResult = Apollo.MutationResult<CreatePostMutation>;
+export type CreatePostMutationOptions = Apollo.BaseMutationOptions<CreatePostMutation, CreatePostMutationVariables>;
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,53 +1,184 @@
-import { Editor } from "../../components/editor";
 import { useRouter } from "next/router";
 import { NextPage } from "next";
-import { usePostQuery } from "../../lib/generated/graphql";
-import { Box, FormControl, FormLabel, Input, Stack } from "@chakra-ui/react";
+import {
+  useCreatePostMutation,
+  usePostQuery,
+} from "../../lib/generated/graphql";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+  Textarea,
+} from "@chakra-ui/react";
+import { ChangeEventHandler, FormEventHandler, useState } from "react";
 
 const Post: NextPage = () => {
   const router = useRouter();
-  const { slug } = router.query;
   let queryValue = "";
-  if (typeof slug === "string") {
-    queryValue = slug;
+  if (typeof router.query.slug === "string") {
+    queryValue = router.query.slug;
   }
 
-  const postQuery = usePostQuery({ variables: { slug: queryValue } });
-  let content = "";
-  if (postQuery.data?.post?.content.body) {
-    content = postQuery.data.post.content.body;
+  const defaultValue = {
+    title: "",
+    date: "",
+    slug: "",
+    coverImage: "",
+    description: "",
+    content: "",
+  };
+  const { data, loading, error } = usePostQuery({
+    variables: { slug: queryValue },
+  });
+  let post = defaultValue;
+  if (data?.post) {
+    const tmp = data?.post;
+    post.title = tmp.title;
+    post.date = tmp.date;
+    post.slug = tmp.slug;
+    post.coverImage = tmp.coverImage;
+    post.description = tmp.description;
+    post.content = tmp.content.body;
   }
+  const [createPostMutation] = useCreatePostMutation();
+
+  if (loading || error) {
+    return null;
+  }
+
+  const handleSubmit = async (post: Post) => {
+    const res = await createPostMutation({
+      variables: {
+        slug: post.slug,
+        title: post.title,
+        coverImage: post.coverImage,
+        content: post.content,
+        description: post.description,
+      },
+    });
+    if (res.errors) {
+      console.error(res.errors);
+    }
+  };
 
   return (
     <Box bg={"white"} p={4}>
-      <Stack spacing={4}>
-        <FormControl id="title">
-          <FormLabel>Title</FormLabel>
-          <Input type="text" />
-        </FormControl>
-        <FormControl id="date">
-          <FormLabel>Date</FormLabel>
-          <Input type="date" />
-        </FormControl>
-        <FormControl id="slug">
-          <FormLabel>Slug</FormLabel>
-          <Input type="text" />
-        </FormControl>
-        <FormControl id="coverImage">
-          <FormLabel>Cover Image</FormLabel>
-          <Input type="text" />
-        </FormControl>
-        <FormControl id="description">
-          <FormLabel>Description</FormLabel>
-          <Input type="text" />
-        </FormControl>
-        <FormControl id="description">
-          <FormLabel>Content</FormLabel>
-          <Editor content={content} />
-        </FormControl>
-      </Stack>
+      <PostForm defaultPost={post} onSubmit={handleSubmit} />
     </Box>
   );
 };
 
 export default Post;
+
+type Post = {
+  title: string;
+  slug: string;
+  date: string;
+  content: string;
+  coverImage: string;
+  description: string;
+};
+
+type Props = {
+  defaultPost: Post;
+  onSubmit: (post: Post) => void;
+};
+const PostForm: React.FC<Props> = ({ defaultPost, onSubmit }) => {
+  const [post, setPost] = useState(defaultPost);
+  const { title, date, slug, content, description, coverImage } = post;
+  const handleTitleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setPost((prev) => {
+      return { ...prev, title: e.target.value };
+    });
+  };
+  const handleDateChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setPost((prev) => {
+      return { ...prev, date: e.target.value };
+    });
+  };
+  const handleSlugChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setPost((prev) => {
+      return { ...prev, slug: e.target.value };
+    });
+  };
+  const handleCoverImageChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setPost((prev) => {
+      return { ...prev, coverImage: e.target.value };
+    });
+  };
+  const handleDescriptionChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setPost((prev) => {
+      return { ...prev, description: e.target.value };
+    });
+  };
+  const handleContentChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    setPost((prev) => {
+      return { ...prev, content: e.target.value };
+    });
+  };
+  const handleSubmit: FormEventHandler = (e) => {
+    console.log("submit");
+    e.preventDefault();
+    onSubmit(post);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <VStack spacing={4}>
+        <FormControl id="title">
+          <FormLabel>Title</FormLabel>
+          <Input type="text" value={title} onChange={handleTitleChange} />
+        </FormControl>
+        <FormControl id="date">
+          <FormLabel>Date</FormLabel>
+          <Input
+            type="date"
+            value={date.split("T")[0]}
+            onChange={handleDateChange}
+          />
+        </FormControl>
+        <FormControl id="slug">
+          <FormLabel>Slug</FormLabel>
+          <Input type="text" value={slug} onChange={handleSlugChange} />
+        </FormControl>
+        <FormControl id="coverImage">
+          <FormLabel>Cover Image</FormLabel>
+          <Input
+            type="text"
+            value={coverImage}
+            onChange={handleCoverImageChange}
+          />
+        </FormControl>
+        <FormControl id="description">
+          <FormLabel>Description</FormLabel>
+          <Input
+            type="text"
+            value={description}
+            onChange={handleDescriptionChange}
+          />
+        </FormControl>
+        <FormControl id="content">
+          <FormLabel>Content</FormLabel>
+          <Textarea
+            minH="800px"
+            value={content}
+            onChange={handleContentChange}
+          />
+        </FormControl>
+        <Button
+          type={"submit"}
+          size="md"
+          bg={"blue.400"}
+          color={"white"}
+          _hover={{
+            bg: "blue.500",
+          }}
+        >
+          Save
+        </Button>
+      </VStack>
+    </form>
+  );
+};
