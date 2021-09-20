1f811535ad9880fddba5e0bc213f5ccdb9ba4355
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2021-09-20 09:36:54 +0900

    add create page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,45 @@
 import type { NextPage } from "next";
-import { Editor } from "../components/editor";
+import { PostForm } from "../components/post-form";
+import { Box } from "@chakra-ui/react";
+import { PostType } from "../types/blog";
+import { useCreatePostMutation } from "../lib/generated/graphql";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
-  return <Editor content={""} />;
+  const router = useRouter();
+  const initialPost: PostType = {
+    id: "",
+    title: "",
+    publishedAt: "",
+    slug: "",
+    coverImage: "",
+    description: "",
+    content: { body: "" },
+  };
+
+  const [createPostMutation] = useCreatePostMutation();
+  const handleSubmit = async (post: PostType) => {
+    const res = await createPostMutation({
+      variables: {
+        title: post.title,
+        publishedAt: post.publishedAt,
+        slug: post.slug,
+        coverImage: post.coverImage,
+        description: post.description,
+        content: post.content.body,
+      },
+    });
+    if (res.errors) {
+      console.error(res.errors);
+    }
+    router.push(`/posts/${post.slug}`);
+  };
+
+  return (
+    <Box bg={"white"} p={4}>
+      <PostForm initialPost={initialPost} onSubmit={handleSubmit} />
+    </Box>
+  );
 };
 
 export default Home;
