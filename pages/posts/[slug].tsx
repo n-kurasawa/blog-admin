560c927e052db190fefff5cd34b695c20cc3a057
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2021-09-22 16:47:28 +0900

    add preview button

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -5,7 +5,7 @@ import {
   useUpdatePostMutation,
 } from "../../lib/generated/graphql";
 import { PostForm } from "../../components/post-form";
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Flex, Stack, useColorModeValue } from "@chakra-ui/react";
 import type { PostType } from "../../types/blog";
 
 const Post: NextPage = () => {
@@ -44,8 +44,33 @@ const Post: NextPage = () => {
   };
 
   return (
-    <Box bg={"white"} p={4}>
-      <PostForm initialPost={data.post} onSubmit={handleSubmit} />
+    <Box bg={"white"} rounded={"md"} boxShadow={"md"}>
+      <Flex
+        minH={"60px"}
+        py={{ base: 2 }}
+        px={{ base: 4 }}
+        borderBottom={1}
+        borderStyle={"solid"}
+        borderColor={"gray.200"}
+        align={"center"}
+      >
+        <Stack
+          flex={{ base: 1 }}
+          direction={"row"}
+          justify={"flex-end"}
+          spacing={6}
+        >
+          <Button fontSize={"sm"} fontWeight={400} variant={"link"} href={"#"}>
+            Preview
+          </Button>
+          <Button fontSize={"sm"} variant={"link"} fontWeight={400} href={"#"}>
+            Form
+          </Button>
+        </Stack>
+      </Flex>
+      <Box p={6}>
+        <PostForm initialPost={data.post} onSubmit={handleSubmit} />
+      </Box>
     </Box>
   );
 };
