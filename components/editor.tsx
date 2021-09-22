23f9877f874b1fd6d4d98ba2b7a5b2308dbbd2fa
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2021-09-22 21:55:25 +0900

    use tab

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Stack } from "@chakra-ui/react";
+import { Box, TabList, Tabs, Tab, TabPanels, TabPanel } from "@chakra-ui/react";
 import { PostForm } from "./post-form";
 import { PostType } from "../types/blog";
 
@@ -10,32 +10,20 @@ type Props = {
 export const Editor: React.FC<Props> = ({ onSubmit, initialPost }) => {
   return (
     <Box bg={"white"} rounded={"md"} boxShadow={"md"}>
-      <Flex
-        minH={"60px"}
-        py={{ base: 2 }}
-        px={{ base: 4 }}
-        borderBottom={1}
-        borderStyle={"solid"}
-        borderColor={"gray.200"}
-        align={"center"}
-      >
-        <Stack
-          flex={{ base: 1 }}
-          direction={"row"}
-          justify={"flex-end"}
-          spacing={6}
-        >
-          <Button fontSize={"sm"} fontWeight={400} variant={"link"} href={"#"}>
-            Preview
-          </Button>
-          <Button fontSize={"sm"} variant={"link"} fontWeight={400} href={"#"}>
-            Form
-          </Button>
-        </Stack>
-      </Flex>
-      <Box p={6}>
-        <PostForm initialPost={initialPost} onSubmit={onSubmit} />
-      </Box>
+      <Tabs>
+        <TabList>
+          <Tab>Form</Tab>
+          <Tab>Preview</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Box p={6}>
+              <PostForm initialPost={initialPost} onSubmit={onSubmit} />
+            </Box>
+          </TabPanel>
+          <TabPanel>preview</TabPanel>
+        </TabPanels>
+      </Tabs>
     </Box>
   );
 };
